8d99bdaa499e33a0f57d5b247eb6521f7ef9eba9
TIAGO-DELL <tiagolbassani@gmail.com>
2020-09-25 11:47:10 -0300

    Port

--- src/server.js
+++ src/server.js
@@ -9,6 +9,6 @@ const routes = require('./routes');
 app.use(express.json());
 app.use(routes);
 
-app.listen(5000, () => {
+app.listen(process.env.APP_PORT || 5000, () => {
   console.log('Server started on port 5000');
 });
