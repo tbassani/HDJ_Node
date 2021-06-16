60d226901bbfd288b31d0cf5347d33a082e4825d
TIAGO-DELL <tiagolbassani@gmail.com>
2021-06-16 09:29:22 -0300

    Return delay

--- src/app/controllers/spotifyController.js
+++ src/app/controllers/spotifyController.js
@@ -360,7 +360,7 @@ module.exports = {
               params: uri_data,
             });
           });
-        await timer(1000);
+        await timer(2000);
       }
       for (const track of tracks) {
         await HDJTracks.update(
