b570f6b33ad1a9a259e64c89f152c97f4d3ca44a
TIAGO-DELL <tiagolbassani@gmail.com>
2021-06-21 19:57:11 -0300

    Retry get playing track

--- src/app/controllers/playlistController.js
+++ src/app/controllers/playlistController.js
@@ -305,29 +305,24 @@ module.exports = {
         method: 'GET',
         url: 'https://api.spotify.com/v1/me/player/currently-playing',
         headers: headers,
-      })
-        .then((resp) => {
-          console.log('Got playing track');
-        })
-        .catch(async (error) => {
-          timer(2000);
-          response = await axios({
-            method: 'GET',
-            url: 'https://api.spotify.com/v1/me/player/currently-playing',
-            headers: headers,
-          })
-            .then((resp) => {
-              console.log('Got playing track');
-            })
-            .catch(async (error) => {
-              timer(2000);
-              response = await axios({
-                method: 'GET',
-                url: 'https://api.spotify.com/v1/me/player/currently-playing',
-                headers: headers,
-              });
-            });
+      });
+
+      if (!response || !response.data || response.data.item) {
+        timer(2000);
+        response = await axios({
+          method: 'GET',
+          url: 'https://api.spotify.com/v1/me/player/currently-playing',
+          headers: headers,
         });
+      }
+      if (!response || !response.data || response.data.item) {
+        timer(2000);
+        response = await axios({
+          method: 'GET',
+          url: 'https://api.spotify.com/v1/me/player/currently-playing',
+          headers: headers,
+        });
+      }
 
       if (response.data.item) {
         playingTrack = {
